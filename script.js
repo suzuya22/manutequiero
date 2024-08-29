2aa032a859db415eff95644d974a2075fd688333
suzuya22 <juandelopezm@gmail.com>
2024-08-29 01:33:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 window.addEventListener('scroll', function() {
-    const header = document.getElementById('header');
     const title = document.getElementById('main-title');
     const buttonsContainer = document.getElementById('buttons-container');
 
-    if (window.scrollY > 100) { // Ajusta el valor según cuándo quieres que empiece la animación
+    if (window.scrollY > 100) {
         title.style.fontSize = '1.5em';
         title.style.textAlign = 'left';
-        title.style.paddingLeft = '20px'; // Ajusta el espaciado a la izquierda según lo necesites
+        title.style.marginLeft = '20px';
         buttonsContainer.style.display = 'flex';
     } else {
         title.style.fontSize = '3em';
         title.style.textAlign = 'center';
-        title.style.paddingLeft = '0';
+        title.style.marginLeft = '0';
         buttonsContainer.style.display = 'none';
     }
 });
